dd7eb1279e3b3ed1614857ffdac795f53a5af5a1
Sophia Valenzuela <svalenzuela29@setoncatholic.org>
2026-09-18 12:25:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 name = input ("What is your name?")
 age= input ("How old are you?")
 color= input ("What is your favorite color?")
-print= ("Hello you are", name ", you are," age "and your favorite color is", color
+print= ("Hello you are", name ", you are," age "and your favorite color is", color")
